b8d5a3afbc4b84b765f8191d04db86c727210edf
Derek <the.calm.flow@gmail.com>
2014-10-22 23:52:56 -0700

    Code Drop

---
+++ Assets/BrettsScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrettsScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(this.gameObject.transform.audio.clip, this.gameObject.transform.position);
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
